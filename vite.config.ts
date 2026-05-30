42cf9b6f3393d9a46a1d619b82bc19e67b0c0cb8
Tejasvi Mehra <tejasvi.mehra@gmail.com>
2026-05-30 18:03:37 +0800

    feat: package module updates and renames

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: "node",
+  },
 });
